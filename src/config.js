3bcccd299f4d04a22391bea6d83f0eb7303b3e1e
mi-yu <michael.yu@wpengine.com>
2018-08-23 00:55:35 -0500

    add results manager heroku url for production app

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_ROOT = process.env.REACT_APP_API_ROOT || ''
-export const RESULTS_MANAGER_API_URL = process.env.RESULTS_MANAGER_API_URL || 'http://localhost:8000'
+export const RESULTS_MANAGER_API_URL = process.env.REACT_APP_RESULTS_MANAGER_API_URL || 'http://localhost:8000'
 
 export default {
 	API_ROOT,
